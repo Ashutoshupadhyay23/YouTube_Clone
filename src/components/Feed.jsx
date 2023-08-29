02681f847c9396b2753fa327b7cd8ac0b27ce988
Ashutosh Upadhyay <upadhyayashutosh423@gmail.com>
2023-08-29 09:13:53 +0530

    first commit

---
+++ src/components/Feed.jsx
@@ -0,0 +1,55 @@
+import { Box, Stack, Typography } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+
+import { fetchFromAPI } from '../utils/fetchFromAPI'
+import SideBar from './SideBar'
+import Videos from './Videos'
+
+const Feed = () => {
+
+  const [selectedCategory, setSelectedCategory] = useState('New');
+  const [videos, setVideos] = useState([]);
+
+
+  useEffect(() => {
+
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+    .then((data) => setVideos(data.items))
+  }, [selectedCategory]);
+  
+
+
+
+  return (
+    <Stack sx={{ flexDirection: { xs: 'column', md: 'row' } }}>
+
+      <Box sx={{ height: { xs:'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: {xs: 0, md: 2} }}>
+
+        <SideBar
+          selectedCategory = {selectedCategory}
+          setSelectedCategory = {setSelectedCategory}
+        />
+
+        <Typography className='copyright' variant='body2' sx={{ mt: 1.5, color: '#fff' }}>
+          Copyright 2023 Shivam
+        </Typography>
+
+      </Box>
+
+      <Box p={2} sx={{ overflow: 'auto', height: '90vh', flex: 2}}>
+        <Typography variant='h4' 
+          fontWeight='bold' mb={2} 
+          sx={{ color: 'white' }}
+        >
+          {selectedCategory} <span style={{ color: '#f31503' }}> Videos</span>
+        </Typography>
+
+        <Videos  videos={videos}/>
+
+      </Box>
+
+    </Stack>
+  )
+}
+
+export default Feed
